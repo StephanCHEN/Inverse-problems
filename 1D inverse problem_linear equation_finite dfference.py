bd7650496855886c0cb4abedfa110ee9799de7a3
StephanCHEN <junfeng.chen@mines-paristech.fr>
2019-03-17 20:11:32 +0100

    Update 1D inverse problem_linear equation_finite dfference.py

--- 1D inverse problem_linear equation_finite dfference.py
+++ 1D inverse problem_linear equation_finite dfference.py
@@ -8,6 +8,8 @@ import time
 n = 100 ##number of intervals
 alpha = np.ones(n) ##initial values
 step = 0.01 ##step size for optimization
+max_iter = 10000000
+tolerance = 1e-6
 
 #write the values of A, B, C as well as x
 B = np.zeros((n, n))
@@ -21,7 +23,7 @@ C = np.zeros(n)
 C[0] = 1.0
 
 x = np.asarray(range(n))/n
-
+###########################################################################3
 def pde_solver(alpha):
         
     return np.linalg.solve(A, np.dot(B, alpha*x)+C)
@@ -41,15 +43,17 @@ def J_grad_alpha(Y, alpha):
     p = np.linalg.solve(A.T, J_grad_Y(Y))
     
     return np.dot(np.dot(B, np.diag(x)).T, p)
-    
+
+def plot_graph(solution, name):
+    plt.subplot(1, 1, 1)
+    plt.plot(x, solution, x, F)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.gca().legend(('Solution','Observation'))
+    plt.savefig('Solution and observation - {}'.format(name))
+###################################################################3
 F = (x + 1)*np.exp(x)
 Y0 = Y = pde_solver(alpha)
-
-plt.subplot(1, 1, 1)
-plt.plot(x, Y0, x, F)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.gca().legend(('Solution','Observation'))
 
 
 iteration = 0
@@ -59,7 +63,7 @@ Y_list = list()
 Y_list.append(Y)
 start_time = time.time()
 
-for k in range(10000000):
+for k in range(max_iter):
     loss.append(Err)
     iteration += 1
     alpha1 = alpha - step * J_grad_alpha(Y, alpha)
@@ -68,16 +72,17 @@ for k in range(10000000):
 
     err = J(Y, F)
     alpha = alpha1
-    if (abs(err-Err)/Err < 1e-7):
+    if (abs(err-Err)/Err < tolerance):
         break
     Err = err
 
 run_time = time.time() - start_time
 loss.append(Err)
 
+plot_graph(Y0, 'original')
+plot_graph(Y, 'optimized')
 
 plt.plot(loss)
-#plt.title('the value of J')
 plt.xlabel('iteration')
 plt.ylabel('J')
 
